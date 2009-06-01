557ecd475b20acec7a7aad66a3c01c55360e5e07
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-06-01 14:34:02 +0100

    Fixed for Rails 2.3

--- lib/html_trace.rb
+++ lib/html_trace.rb
@@ -2,8 +2,9 @@ module ActionView
   module Partials
     private
     # render :partial => "item", :collection => @items
-    def render_partial_with_html_trace(partial_path, *args)
-      "<!-- #{partial_path.upcase} >>> -->\n" + render_partial_without_html_trace(partial_path, *args) + "\n<!-- <<< #{partial_path.downcase} -->"
+    def render_partial_with_html_trace(partial_opts, *args)
+      partial_path = partial_opts[:partial] # better will be use template methods, but this provides just hash
+      "<!-- #{partial_path.upcase} >>> -->\n" + render_partial_without_html_trace(partial_opts, *args) + "\n<!-- <<< #{partial_path.downcase} -->"
     end
     alias_method_chain :render_partial, :html_trace
   end
@@ -11,10 +12,11 @@ end
 
 module ActionController
   class Base
-    # render_for_file(template_path, status = nil, use_full_path = false, locals = {})
-    def render_for_file_with_html_trace(template_path, *args)
-      "\n<!-- #{template_path.upcase} >>> -->\n" + render_for_file_without_html_trace(template_path, *args) + "\n<!-- <<< #{template_path.downcase} -->"
+    # render_for_file(template, status = nil, use_full_path = false, locals = {})
+    def render_for_file_with_html_trace(template, *args)
+      template_path = template.relative_path
+      "\n<!-- #{template_path.upcase} >>> -->\n" + render_for_file_without_html_trace(template, *args) + "\n<!-- <<< #{template_path.downcase} -->"
     end
     alias_method_chain :render_for_file, :html_trace
   end
-end    
+end
